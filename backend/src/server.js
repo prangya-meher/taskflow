e854df657b9e8244bffb41ebcbb47194d572e464
Prangya <meherprangya@gmail.com>
2026-08-16 01:28:23 +0530

    db connected port

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,11 @@ const taskRoutes = require("./routes/taskRoutes");
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
 
 app.use(cors());
 app.use(express.json());
